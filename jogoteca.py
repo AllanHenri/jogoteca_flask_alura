a52478a0d1e27c96b4a6c3f16f12209e7e88fc90
Allan Moura <116675091+AllanHenri@users.noreply.github.com>
2025-08-04 14:41:19 -0300

    Adicionando arquivos

---
+++ jogoteca.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console = console
+@app.route('/inicio')
+def ola():
+    jogo1= Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2= Jogo('God of war', 'Rack n Slash','PS2')
+    jogo3 = Jogo('MK', 'luta', 'Arcade')
+    jogos = [jogo1,jogo2,jogo3]
+    return render_template('lista.html', titulo = 'Jogos', jogos = jogos)
+
+app.run()
